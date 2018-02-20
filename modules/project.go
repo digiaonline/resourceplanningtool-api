4abbb2ab53abec2d890f08302d73acd466a7a001
Erik Veijola <erik.veijola@digia.com>
2018-02-20 15:44:42 +0200

    project: add struct field picture
    Signed-off-by: Erik Veijola <erik.veijola@digia.com>

--- modules/project.go
+++ modules/project.go
@@ -4,14 +4,15 @@ type Project struct {
 	ID		int
 	Name		string
 	Description	string
+	Picture		string
 }
 
 func InsertProject(project *Project) error {
 	var id int
-	err := db.QueryRow(`INSERT INTO project (name, description)
-			    VALUES ($1, $2)
+	err := db.QueryRow(`INSERT INTO project (name, description, picture)
+			    VALUES ($1, $2, $3)
 			    RETURNING id`,
-			    project.Name, project.Description).Scan(&id)
+			    project.Name, project.Description, project.Picture).Scan(&id)
 	if err != nil {
 		return err
 	}
@@ -20,17 +21,12 @@ func InsertProject(project *Project) error {
 }
 
 func GetProjectByID(id int) (*Project, error) {
-	var name, description string
-	err := db.QueryRow(`SELECT name, description FROM project where id=$1`, id).Scan(&name, &description)
+	project := Project{}
+	err := db.QueryRowx(`SELECT * FROM project where id=$1`, id).Scan(&project)
 	if err != nil {
 		return nil, err
 	}
-	return &Project{
-		ID:		id,
-		Name:		name,
-		Description:	description,
-	}, nil
-	return nil, nil
+	return &project, nil
 }
 
 func RemoveProjectByID(id int) error {
@@ -49,26 +45,22 @@ func RemoveFromProject(project_id, person_id int) error {
 }
 
 func GetUsersInProjectByID(id int) ([]*Person, error) {
-	rows, err := db.Query(`SELECT pers.id, pers.name, pers.email, pers.description
+	rows, err := db.Queryx(`SELECT pers
 			       FROM person AS pers, worksinproject AS wproj
 			       WHERE pers.id = wproje.person_id
 			       AND wproj.project_id=$1`, id)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
-	var (
-		persons		= []*Person{}
-		person_id	int
-		name		string
-		email		string
-		description	string
-	)
+
+	var persons = []*Person{}
+
 	for rows.Next() {
-		if err = rows.Scan(&person_id, &name, &email, &description); err != nil {
+		person := Person{}
+		if err = rows.StructScan(&person); err != nil {
 			return nil, err
 		}
-		persons = append(persons, &Person{ID: person_id, Name: name, Email: email, Description: description})
+		persons = append(persons, &person)
 	}
 	return persons, nil
 }
